eb2a7333245388ec78176ce10bbe2bcfca77f59d
lankabelgezogen <cybertom1234@gmail.com>
2024-02-21 12:51:12 +0100

    deleted database_test.py

--- application/backend/chatbot/database_test.py
+++
@@ -1,9 +0,0 @@
-from history import PostgresChatMessageHistory
-
-history = PostgresChatMessageHistory(session_id="test_session_id")
-
-history.add_user_message("hi!")
-
-history.add_ai_message("whats up?")
-
-print(history.messages)
