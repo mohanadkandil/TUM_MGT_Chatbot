35a9e803acba6f6d4866258a56d8335ed66b5edc
mkandil <mohanadmkandil@gmail.com>
2024-03-07 16:20:39 +0200

    fix: conversation endpoint change

--- application/frontend/src/components/chat.tsx
+++ application/frontend/src/components/chat.tsx
@@ -54,7 +54,7 @@ export function Chat() {
     setInput("");
 
     const encodedQuestion = encodeURIComponent(input);
-    const url = `http://64.227.113.135:8000/conversation?question=${encodedQuestion}`;
+    const url = `${process.env.NEXT_PUBLIC_CONVERSATIONAPI}?question=${encodedQuestion}`;
 
     try {
       const response = await fetch(url, {
