d85e05161d2c7f2f4f5e24175b8e7528e5fa9029
benediktbluemelhuber <benedikt.bluemelhuber@tum.de>
2024-03-19 20:49:41 +0100

    refactoring of api

--- application/backend/api/api.py
+++ application/backend/api/api.py
@@ -3,9 +3,10 @@ import os
 from operator import itemgetter
 from application.backend.chatbot.chatbot import Chatbot, Message, Conversation
 from dotenv import find_dotenv, load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+from pydantic import BaseModel, Field
 
 load_dotenv(find_dotenv())
 
@@ -27,18 +28,52 @@ app.add_middleware(
 )
 
 
+# Define the request model for feedback
+class Feedback(BaseModel):
+    uuid: str = Field(
+        ..., description="The unique identifier for the feedback session as a string"
+    )
+    feedback_classification: str = Field(
+        ..., description="The classification of the feedback"
+    )
+    feedback_text: str = Field(..., description="The detailed feedback text")
+
+
 @app.get("/test")
 async def test():
     return {"test": "works"}
 
 
 @app.post("/conversation")
-async def ask_question(question: str, conversation: Conversation) -> dict:
+async def ask_question(
+    question: str, conversation: Conversation, study_program: str = ""
+) -> dict:
     print(question)
     print(conversation)
-    answer = bot.chat(question=question, conversation=conversation)
+    answer = bot.chat(
+        question=question, conversation=conversation, study_program=study_program
+    )
 
     return {"answer": answer}
+
+
+@app.post("/chat_stream/")
+async def chat_stream_endpoint(
+    question: str, conversation: Conversation, study_program: str = ""
+):
+    return StreamingResponse(
+        bot.chat_stream(
+            question=question, conversation=conversation, study_program=study_program
+        ),
+        media_type="text/event-stream",
+    )
+
+
+@app.post("/feedback")
+async def send_feedback(feedback: Feedback):
+    # logging to write feedback to db.
+    logger.info(f"Feedback received: {feedback}")
+    return {"message": "Feedback received successfully"}
 
 
 if __name__ == "__main__":
